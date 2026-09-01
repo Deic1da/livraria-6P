bdcf0c0ae963b82b139fddeb24f762ec315d93d9
Deic1da <melquenerd@gmail.com>
2026-09-01 13:28:42 -0300

    Começando os trabalhos!!!

---
+++ src/test/java/br/com/melqui/livraria/LivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.melqui.livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
